1915c3357924d9599eba05fc4e44a64197912ecb
aditya-sharma02 <adyashma786@gmail.com>
2023-05-28 00:05:35 +0530

    second commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller()
 export class AuthController {
     constructor(private authservice: AuthService) { }
     @Post('/signup')
-    signup(@Body('NAME') name: string) {
-        return this.authservice.signup(name)
+    signup(@Body() dto: AuthDto) {
+        return this.authservice.signup(dto)
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from "@nestjs/common";
+import { AuthDto } from "./dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "src/interface/all.interface";
 
 @Injectable({})
-export class AuthService { 
-    
-    signup(name:string){
-        return name;
+export class AuthService {
+    constructor(@InjectModel("User") private userModel: Model<User>) { }
+    async signup(dto: AuthDto) {
+        try {
+            const newobj = new this.userModel(dto)
+            const data = await newobj.save()
+            return data;
+        } catch (e) {
+            if (e.code === 11000){
+                return "credentials taken already"
+            }
+            return e
+        }
     }
 }
